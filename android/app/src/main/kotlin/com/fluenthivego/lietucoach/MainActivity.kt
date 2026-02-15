a04c9fdbd64ac15f4b51d3c6ab245c1490a8ed2a
Shahadat Hossain <mdshahadat196@gmail.com>
2026-02-16 00:10:06 +0200

    fix(android): resolve deep link callback issues by using fully qualified MainActivity name and singleTask launchMode. Added deep link logging.

--- android/app/src/main/kotlin/com/fluenthivego/lietucoach/MainActivity.kt
+++ android/app/src/main/kotlin/com/fluenthivego/lietucoach/MainActivity.kt
@@ -6,9 +6,24 @@ import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodChannel
 
+import android.Manifest // Not needed but avoiding breaking imports
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+
 class MainActivity : FlutterActivity() {
     private val METHOD_CHANNEL = "app.lietucoach/pad"
     private val EVENT_CHANNEL = "app.lietucoach/pad_progress"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("OAUTH", "onCreate data=" + intent?.dataString)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        Log.d("OAUTH", "onNewIntent data=" + intent.dataString)
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
